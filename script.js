feac144b7ccab7030c3cee8c1b2ebc338d150bff
Sungpil An <ansungpil1@gmail.com>
2020-12-13 13:41:12 -0500

    Added declared variables in script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+//Variables for questions & answers
+var questions = [
+    {
+        title: "What are the commonly used data types that Not included:",
+        choice: ["string", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }
+    {
+        title: "What is the condition in an if/else statement is enclosed within:",
+        choice: ["quotes", "curly brackets", "parenthesis", "square brakets"],
+        answer: "parentheses"
+    }
+    {
+        title: "Arrays in Javascript can be used to store:",
+        choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    }
+    {
+        title: "String values must be enclosed within____when being assigned to variables.",
+        choice: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    }
+    {
+        title: "A very useful tool for used during development and debugging for printing content to:",
+        choice: ["Javascript", "terminal/bash", "for loops", "console log"],
+        answer: "console log"
+    },
+];
+//Variables to declare
+var score = 0;
+var questionList = 0;
+
