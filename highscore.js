97d225ca831778b40563ae5e6c677b5573c0a996
Sungpil An <ansungpil1@gmail.com>
2020-12-13 20:06:01 -0500

    Added few comments and added function to move back to index page upon clearing

--- highscore.js
+++ highscore.js
@@ -1,17 +1,22 @@
+//Static variables
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var previous = document.querySelector("#previous");
-
+//Function to clear the score.
 clear.addEventListener("click", function() {
     localStorage.clear();
     localStorage.reload();
 });
 var scoreList = localStorage.getItem("scoreList");
 scoreList = JSON.parse (scoreList);
-
+//Retrieving local storage
 if (scoreList !== null) {
     for (var i = 0; i < scoreList.length; i++) {
-        HTMLDataListElement.textContent = scoreList[i].initials + " " + listScore[i].score;
-        
+       liEl.textContent = scoreList[i].initials + " " + listScore[i].score;
+        highScore.appendChild(liEl);
     }
 }
+//function to move back to index page
+previous.addEventListener("click", function() {
+    window.location.replace("index.html");
+});
